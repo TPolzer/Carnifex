0736e55567ce1ecdadb5d714cfadec6359f35640
Tobias Polzer <tobias.polzer@fau.de>
2016-05-10 20:42:56 +0200

    Göttingen coat of arms svg version

--- src/client/artwork/mapping.js
+++ src/client/artwork/mapping.js
@@ -3,7 +3,7 @@ function coats() {
 		'Universität des Saarlandes': 'artwork/Wappen_des_Saarlands.svgz',
 		'FAU Erlangen-Nürnberg': 'artwork/Wappen_Erlangen.svgz',
 		'Reykjavik University': 'artwork/ISL_Reykjavik_COA.svgz',
-		'Georg-August-Universität Göttingen': 'artwork/Stadtwappen_Goettingen.PNG',
+		'Georg-August-Universität Göttingen': 'artwork/DEU_Goettingen_COA.svgz',
 		'TU München': 'artwork/Muenchen_Kleines_Stadtwappen.svgz',
 		'Karlsruher Institut für Technologie': 'artwork/Coat_of_arms_de-bw_Karlsruhe.svgz',
 	};
