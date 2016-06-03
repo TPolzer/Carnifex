dce86ba9583d0a06b33361306fc944795f9ede51
Tobias Polzer <tobias.polzer@fau.de>
2016-06-03 22:30:23 +0200

    Mooooare coats of arms (GCPC)

--- src/client/artwork/mapping.js
+++ src/client/artwork/mapping.js
@@ -6,6 +6,7 @@ function coats() {
 		'Reykjavik University': 'artwork/ISL_Reykjavik_COA.svgz',
 		'Georg-August-Universität Göttingen': 'artwork/DEU_Goettingen_COA.svgz',
 		'TU München': 'artwork/Muenchen_Kleines_Stadtwappen.svgz',
+		'Technische Universität München': 'artwork/Muenchen_Kleines_Stadtwappen.svgz',
 		'Karlsruher Institut für Technologie': 'artwork/Coat_of_arms_de-bw_Karlsruhe.svgz',
 		'Rheinische Friedrich-Wilhelms-Universit\u00e4t Bonn': 'artwork/DEU_Bonn_COA.svgz',
 		'Technische Universit\u00e4t Dortmund': 'artwork/Coat_of_arms_of_Dortmund.svgz',
@@ -16,6 +17,8 @@ function coats() {
 		'Julius-Maximilians-Universit\u00e4t W\u00fcrzburg': 'artwork/Wappen_von_Wuerzburg.svgz',
 		'Universit\u00e4t zu L\u00fcbeck': 'artwork/Wappen_Lübeck.svgz',
 		'Universit\u00e4t Osnabr\u00fcck': 'artwork/Osnabrück_Wappen.svgz',
+		'Hasso-Plattner-Institut Potsdam': 'artwork/Coat_of_arms_of_Potsdam.svgz',
+		'Technische Universit\u00e4t Kaiserslautern': 'artwork/Kaiserslautern-Stadtwappen.svgz',
 	};
 	return function (aff) {
 		var res = m[aff];
